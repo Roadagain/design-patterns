b7f55eea473d5461b74f1625e5cab8b1341b4ddf
Roadagain <ringoh72@gmail.com>
2017-12-19 09:58:55 +0900

    Insert an empty line between two methods

--- src/org/roadagain/designpattern/Singleton.java
+++ src/org/roadagain/designpattern/Singleton.java
@@ -2,6 +2,7 @@ public class Singleton {
     private static Singleton instance = null;
 
     private Singleton(){}
+
     public Singleton getInstance(){
         if (instance == null){
             instance = new Singleton();
